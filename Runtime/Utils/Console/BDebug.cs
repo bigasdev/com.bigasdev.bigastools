40c59117c238e9e08364d28279dc8fdaeaa869e6
gustavo domingues <deathdiec@gmail.com>
2022-04-12 19:32:28 -0300

    fix: finally release

--- Runtime/Utils/Console/BDebug.cs
+++ Runtime/Utils/Console/BDebug.cs
@@ -6,7 +6,7 @@ public static class BDebug
 {
     //
     // Use this to debug log with color and a "title" it's useful so you can search by logs with the same title;
-    // To check logs related to some class you are testing.
+    // To check logs related to some class you are testing;
     // 
     public static void Log(object message, string title = "Log", Color? color = null){
        var c = color == null ? Color.yellow : color.Value;
